dd0d1d8fe7cd926b105447212a770637926d88f1
Abid Mir <abidmir487@gmail.com>
2025-05-19 00:02:00 +0530

    Create webserver.py

--- Procfile
+++
@@ -1,1 +0,0 @@
-worker: python MarketWatchdog.py
---
+++ webserver.py
@@ -0,0 +1,16 @@
+# webserver.py
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def start_webserver():
+    t = Thread(target=run)
+    t.start()
