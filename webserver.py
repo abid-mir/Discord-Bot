8a2d73c54afa83bb3ec05a4ccb82ebd9e67fd925
Abid Mir <abidmir487@gmail.com>
2025-05-19 13:18:40 +0530

    new

--- webserver.py
+++ webserver.py
@@ -9,7 +9,9 @@ def home():
     return "Bot is running!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    # app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=False, use_reloader=False, threaded=True)
+
 
 def start_webserver():
     t = Thread(target=run)
